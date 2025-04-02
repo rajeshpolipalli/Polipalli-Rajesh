ffd3a93734d5053eb15bb24c8a084f9700294a8c
POLIPALLI RAJESH <158044178+rajeshpolipalli@users.noreply.github.com>
2025-04-03 05:20:36 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Frontend Development", "Backend Development", "Embedded Systems Engineer."],
+    strings: ["Frontend Developer", "Backend Developer", "Embedded Systems Engineer."],
     loop: true,
     typeSpeed: 30,
     backSpeed: 20,
