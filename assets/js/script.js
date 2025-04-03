e5a0c563a480ecdfeac17d09b584e2291652b9e3
POLIPALLI RAJESH <158044178+rajeshpolipalli@users.noreply.github.com>
2025-04-03 09:36:27 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,19 +39,20 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
 
-        $("#contact-form").submit(function (event) {
-            event.preventDefault(); // Prevent default form submission
+    $("#contact-form").submit(function (event) {
+        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
 
-            emailjs.sendForm('contact_service', 'template_contact', this)
-                .then(function (response) {
-                    console.log('SUCCESS!', response.status, response.text);
-                    document.getElementById("contact-form").reset();
-                    alert("Form Submitted Successfully");
-                })
-                .catch(function (error) {
-                    console.log('FAILED...', error);
-                    alert("Form Submission Failed! Try Again");
-                });
+        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                document.getElementById("contact-form").reset();
+                alert("Form Submitted Successfully");
+            }, function (error) {
+                console.log('FAILED...', error);
+                alert("Form Submission Failed! Try Again");
+            });
+        event.preventDefault();
+    });                                                                                                                                                                           
 
 
 
